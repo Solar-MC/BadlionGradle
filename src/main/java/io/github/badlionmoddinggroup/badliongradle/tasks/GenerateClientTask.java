d940515190d5d23e6526f851c18af2e87266579c
hYdos <haydenv06@gmail.com>
2020-07-07 15:44:01 +1000

    1.1 bump version

--- src/main/java/io/github/badlionmoddinggroup/badliongradle/tasks/GenerateClientTask.java
+++ src/main/java/io/github/badlionmoddinggroup/badliongradle/tasks/GenerateClientTask.java
@@ -1,6 +1,17 @@
 package io.github.badlionmoddinggroup.badliongradle.tasks;
 
 import org.gradle.api.DefaultTask;
+import org.gradle.api.tasks.TaskAction;
 
 public class GenerateClientTask extends DefaultTask {
+
+    public GenerateClientTask() {
+        setGroup("patching");
+    }
+
+    @TaskAction
+    public void run(){
+
+    }
+
 }
--- src/main/java/io/github/badlionmoddinggroup/badliongradle/tasks/GrabVersionInfo.java
+++ src/main/java/io/github/badlionmoddinggroup/badliongradle/tasks/GrabVersionInfo.java
@@ -1,6 +1,17 @@
 package io.github.badlionmoddinggroup.badliongradle.tasks;
 
 import org.gradle.api.DefaultTask;
+import org.gradle.api.tasks.TaskAction;
 
 public class GrabVersionInfo extends DefaultTask {
+
+    public GrabVersionInfo() {
+        setGroup("patching");
+    }
+
+    @TaskAction
+    public void run(){
+
+    }
+
 }
