9add5d970df4f2385eff3e33de075fc15b56a8ee
hayden <haydenv06@gmail.com>
2020-08-23 12:47:33 +1000

    better results for BCP

--- src/main/java/io/github/badlionmoddinggroup/badliongradle/task/SetupBCPTask.java
+++ src/main/java/io/github/badlionmoddinggroup/badliongradle/task/SetupBCPTask.java
@@ -3,6 +3,7 @@ package io.github.badlionmoddinggroup.badliongradle.task;
 import io.github.badlionmoddinggroup.badliongradle.BadlionGradle;
 import io.github.badlionmoddinggroup.badliongradle.provider.BadlionProvider;
 import org.gradle.api.DefaultTask;
+import org.gradle.api.file.RegularFileProperty;
 import org.gradle.api.tasks.TaskAction;
 import org.jetbrains.java.decompiler.main.decompiler.ConsoleDecompiler;
 import org.zeroturnaround.zip.ZipUtil;
@@ -12,10 +13,10 @@ import java.nio.file.Path;
 
 public class SetupBCPTask extends DefaultTask {
 
+
     public SetupBCPTask() {
         setGroup("patching");
     }
-
 
     public File getDecompilePath(String blcVersion) {
         return new File(BadlionGradle.getVersionCache(getProject(), blcVersion), "decompile/");
@@ -40,7 +41,7 @@ public class SetupBCPTask extends DefaultTask {
     private void decompileClient(BadlionProvider badlion) {
         Path strippedMinecraftOutput = BadlionGradle.getVersionCacheFile(getProject(), badlion.badlionVersion, "badlionRemappedNamed.jar").toPath();
         getDecompilePath(badlion.badlionVersion).mkdirs();
-        ConsoleDecompiler.main(new String[]{strippedMinecraftOutput.toString(), getDecompilePath(badlion.badlionVersion).getAbsolutePath()});
+        ConsoleDecompiler.main(new String[]{"-din=1", "-rbr=0", "-dgs=1", "-asc=1", "-rsy=0", "-iec=1", "-jvn=1", "-isl=0", "-iib=1", "-log=TRACE", "-ind=    ", strippedMinecraftOutput.toString(), getDecompilePath(badlion.badlionVersion).getAbsolutePath()});
     }
 
 }
